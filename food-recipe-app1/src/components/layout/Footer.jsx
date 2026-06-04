9cacacfcc381645e28a15df6e40d57a498a6b7b6
SalahVincent <vincentsalah3@gmail.com>
2026-06-04 11:03:02 +0100

    style footer section with link to rebase code camp website

--- food-recipe-app1/src/components/layout/Footer.jsx
+++ food-recipe-app1/src/components/layout/Footer.jsx
@@ -2,7 +2,8 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <a href='https://www.rebasecodecamp.com/' className='px-10 my-10 flex justify-center text-gray-400'>Rebase Code Camp</a>
+    // Vincent Salah
   )
 }
 
